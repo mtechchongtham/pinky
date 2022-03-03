91f6b7c66b5a44658ee535cf14db86c0966e2853
mtechchongtham <82599997+mtechchongtham@users.noreply.github.com>
2022-03-03 19:26:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def index():
 
         if file and allowed_files(file.filename):
             filename = secure_filename(file.filename)
-            file.save(os.path.join(BASE_PATH, '/Users/chongthamkaj/OneDrive - Microsoft 365/pythonscript/sushila/upload', filename))
+            file.save(os.path.join(BASE_PATH, '/upload', filename))
             flash('File Uploaded Successully')
             return redirect(url_for('download'))
 
